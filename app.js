ef6e4cdc44bb0574d830195aee80416f088950ec
Kickblip <wyatt.zilker@gmail.com>
2023-01-27 15:20:11 -0600

    you can download videos now

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const { google } = require("googleapis");
+const youtubedl = require('youtube-dl-exec')
 
 const API_KEY = process.env.API_KEY;
 
@@ -22,8 +23,13 @@ app.get('/search', (req, res) => {
     youtube.search.list({
         part: 'snippet',
         q: searchQuery,
-        maxResults: 5
+        maxResults: 5,
+        type: "video",
+        videoDuration: "short"
+
     }).then(response => {
+        console.log(`first video id: ${response.data.items[0].id.videoId}`);
+        downloadVideoContent(response.data.items[0].id.videoId);
         res.send({
             'result': response.data.items
         });
@@ -36,7 +42,37 @@ app.get('/search', (req, res) => {
     });
 });
 
+app.get('/download', (req, res) => {
+
+    const testId = '4In8wOZreGQ';
+    console.log('attempting to download content...');
+    downloadVideoContent(testId);
+
+});
 
 
-console.log('listening on 8888');
-app.listen(8888);
+const downloadVideoContent = (videoId) => {
+    youtubedl(`https://www.youtube.com/watch?v=${videoId}`, {
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: [
+            'referer:youtube.com',
+            'user-agent:googlebot'
+        ]
+
+    }).then((output) => {
+        console.log(output);
+    });
+}
+
+
+
+app.listen(8888, () => {
+    console.log('listening on 8888')
+});
+
+
+// 4In8wOZreGQ
+
+// https://youtu.be/RmOq1pNzzwI
