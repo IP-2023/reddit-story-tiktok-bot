0c332a20519a307da005850a3a9ab13ad9011630
Kickblip <wyatt.zilker@gmail.com>
2023-01-28 12:07:41 -0600

    add form

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const { google } = require("googleapis");
 const youtubedl = require('youtube-dl-exec')
+// youtubedl require python 3.7+
 
 const API_KEY = process.env.API_KEY;
 
-
 const app = express();
 app.use(express.static(__dirname + '/public'));
-
 
 const youtube = google.youtube({
     version: 'v3',
@@ -28,7 +27,6 @@ app.get('/search', (req, res) => {
         videoDuration: "short"
 
     }).then(response => {
-        console.log(`first video id: ${response.data.items[0].id.videoId}`);
         downloadVideoContent(response.data.items[0].id.videoId);
         res.send({
             'result': response.data.items
@@ -44,14 +42,16 @@ app.get('/search', (req, res) => {
 
 app.get('/download', (req, res) => {
 
-    const testId = '4In8wOZreGQ';
-    console.log('attempting to download content...');
-    downloadVideoContent(testId);
+    const videoId = req.query.video_id;
+
+    downloadVideoContent(videoId);
 
 });
 
 
 const downloadVideoContent = (videoId) => {
+    console.log(`attempting to download video ${videoId}`);
+
     youtubedl(`https://www.youtube.com/watch?v=${videoId}`, {
         noCheckCertificates: true,
         noWarnings: true,
@@ -61,18 +61,11 @@ const downloadVideoContent = (videoId) => {
             'user-agent:googlebot'
         ]
 
-    }).then((output) => {
-        console.log(output);
     });
-}
+};
 
 
 
 app.listen(8888, () => {
     console.log('listening on 8888')
 });
-
-
-// 4In8wOZreGQ
-
-// https://youtu.be/RmOq1pNzzwI
