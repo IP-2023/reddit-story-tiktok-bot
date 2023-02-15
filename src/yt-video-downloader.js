832c8be6b10fe1c1c5140daadb2f3a1fd5fdf32e
Kickblip <wyatt.zilker@gmail.com>
2023-02-15 12:14:52 -0600

    trying to download partial videos

--- src/yt-video-downloader.js
+++ src/yt-video-downloader.js
@@ -1,29 +1,24 @@
 const youtubedl = require('youtube-dl-exec');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
 // youtubedl require python 3.7+
 //n_Dv4JMiwK8
+// short:  JYs_94znYy0
 
 const downloadVideoContent = async (videoId) => {
     console.log(`attempting to download video ${videoId}`);
-    const videoURL = `https://www.youtube.com/watch?v=${encodeURIComponent(videoId)}`;
 
+    const options = {
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: [
+            'referer:youtube.com',
+            'user-agent:googlebot'
+        ],
+        output: `${videoId}.mp4`,
+        downloadSections: '*1:15-inf' // Download seconds 93 to 111
+    };
 
-    // youtubedl(`https://www.youtube.com/watch?v=${videoId}`, {
-    //     noCheckCertificates: true,
-    //     noWarnings: true,
-    //     preferFreeFormats: true,
-    //     addHeader: [
-    //         'referer:youtube.com',
-    //         'user-agent:googlebot'
-    //     ]
-
-    // });
-    // Get video information
-    // Get video information
-
-
-
+    youtubedl(`https://www.youtube.com/watch?v=${videoId}`, options).catch((error) => { console.log(error) });
 
 };
 
